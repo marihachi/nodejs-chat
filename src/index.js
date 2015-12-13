96752bb4c19081cc88ead17e2b05478620381e33
Petitsurume <gamemannew2013@yahoo.co.jp>
2015-12-13 19:15:58 +0900

    申し訳ないがhostとaddressを間違うのはNG

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ io.on('connection', function(socket){
 		name=_name;
 		joincount++;
 		socket.emit("join");
-		console.log("join",name,socket.handshake.headers.host);
+		console.log("join",name,socket.handshake.headers.address);
 		io.emit("system",name+"さんが入室しました");
 		io.emit("updateinfo");
 		socket.on("chat message",function(msg){
